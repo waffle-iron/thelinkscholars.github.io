67f6bcf85ce438090340c3b17a3efb192a8769ee
hexrdesign <support@hexr.org>
2016-08-25 18:47:54 -0400

    Index is pretty much done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 $(".parallax").parallax();
 $(".carousel").carousel({padding:100,shift:100})
-$(".slider").slider({interval:1000});
+$(".slider:not(.home-slider)").slider({interval:1000});
+$(".home-slider").slider({indicators:false,full_width:true});
 $(".slides").hover(function(){$(".slider").slider('pause');},function(){$(".slider").slider('start');})
