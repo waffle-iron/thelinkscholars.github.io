bce92224a69415eb09795361b92fbace94476fe1
hexrdesign <support@hexr.org>
2016-08-24 21:04:16 -0400

    pre-meeting push. Not stable

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,4 @@
 $(".parallax").parallax();
+$(".carousel").carousel({padding:100,shift:100})
+$(".slider").slider({interval:1000});
+$(".slides").hover(function(){$(".slider").slider('pause');},function(){$(".slider").slider('start');})
