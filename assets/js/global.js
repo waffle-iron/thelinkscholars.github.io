c7fff16da87916f547f3cf2bc25f9a8579ff1cdb
hexrdesign <hexrdesign@gmail.com>
2016-10-17 19:24:50 -0400

    Fixed position menu that disappears down scroll; index updates

--- assets/js/global.js
+++ assets/js/global.js
@@ -8,4 +8,14 @@ $(document).ready(function()
 			scrollTop: $($(this).attr("href")).offset().top
 		}, 1000);
 	});
+	window.position = 0;
+	$(window).scroll(function(event){
+		var st = $(this).scrollTop();
+		if (st > window.position){
+			$("header").css("opacity","0")
+		} else {
+			$("header").css("opacity","1")
+		}
+		window.position = st;
+	});
 });
